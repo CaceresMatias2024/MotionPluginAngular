2bcc0752c2e30a4000a4e03c942d3e97170286a1
matias.cacerestello <matias.cacerestello@gmail.com>
2025-03-26 16:54:43 +0000

    pantalusecondcomit

--- src/app/components/motion/motion.component.ts
+++ src/app/components/motion/motion.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MotionService } from './Services/motion.service';
 import { MotionData } from './Model/MotionData.model';
 
 @Component({
   selector: 'app-motion',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './motion.component.html',
   styleUrl: './motion.component.scss'
 })
 export class MotionComponent implements OnInit, OnDestroy {
-  
-  constructor(private motionS: MotionService) {}
-
   motionData: MotionData = {};
+
+  constructor(private motionS: MotionService) {}
 
   ngOnInit(): void {
     this.motionS.startMotionDetection((data: MotionData) => {
